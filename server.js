f6d0fa88bbbb4e72ac86af71083b85611c61ffad
Pfizz <pfiste80@gmail.com>
2021-09-20 17:20:32 -0400

    End of lesson 4

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
+const helpers = require('./utils/helpers');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 const session = require('express-session');
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -18,10 +19,10 @@ const sess = {
   })
 };
 
-
-
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
