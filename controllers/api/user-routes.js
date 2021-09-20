203b643655928f2adabb720a27b3f3e4625874f5
Pfizz <pfiste80@gmail.com>
2021-09-20 15:45:43 -0400

    3

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -143,4 +143,16 @@ router.delete('/:id', (req, res) => {
     });
 });
 
+// logic for log out
+router.post('/logout', (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  }
+  else {
+    res.status(404).end();
+  }
+});
+
 module.exports = router;
